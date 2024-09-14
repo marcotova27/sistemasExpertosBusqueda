568f15a964d3759610d96ed0b1fc1e17cab6011b
marcotova27 <a21310113@ceti.mx>
2024-09-14 08:03:07 -0600

    Update Ancho.py

--- Ancho.py
+++ Ancho.py
@@ -2,52 +2,54 @@ class Grafo:
     def __init__(self):
         self.grafo = {}
 
-    def agregar_arista(self, nodo1, nodo2):
+    def agregar_arista(self, nodo1, nodo2, distancia):
         if nodo1 not in self.grafo:
             self.grafo[nodo1] = []
-        self.grafo[nodo1].append(nodo2)
+        self.grafo[nodo1].append((nodo2, distancia))
 
         if nodo2 not in self.grafo:
             self.grafo[nodo2] = []
-        self.grafo[nodo2].append(nodo1)
+        self.grafo[nodo2].append((nodo1, distancia))
 
     def bfs(self, inicio, objetivo):
         visitado = set()
-        cola = [[inicio]]
+        cola = [[(inicio, 0)]]
 
         if inicio == objetivo:
-            return [inicio]
+            return [inicio], 0
 
         while cola:
             camino = cola.pop(0)
-            nodo = camino[-1]
+            nodo, distancia_total = camino[-1]
 
             if nodo not in visitado:
-                for vecino in self.grafo.get(nodo, []):
+                for vecino, distancia in self.grafo.get(nodo, []):
                     nuevo_camino = list(camino)
-                    nuevo_camino.append(vecino)
+                    nuevo_camino.append((vecino, distancia_total + distancia))
                     cola.append(nuevo_camino)
 
                     if vecino == objetivo:
-                        return nuevo_camino
+                        return [n[0] for n in nuevo_camino], nuevo_camino[-1][1]
                 visitado.add(nodo)
 
-        return None
+        return None, None
 
+# Crear el grafo
 grafo = Grafo()
-# Agregar nodos
-grafo.agregar_arista('CETI', 'Plaza de la Liberación')
-grafo.agregar_arista('CETI', 'Teatro Degollado')
-grafo.agregar_arista('Plaza de la Liberación', 'Templo Expiatorio')
-grafo.agregar_arista('Teatro Degollado', 'Templo Expiatorio')
-grafo.agregar_arista('Teatro Degollado', 'Rotonda de los Jaliscienses Ilustres')
-grafo.agregar_arista('Templo Expiatorio', 'Rotonda de los Jaliscienses Ilustres')
-grafo.agregar_arista('Instituto Cultural Cabañas', 'Plaza de la Liberación')
-grafo.agregar_arista('Instituto Cultural Cabañas', 'Plaza Tapatía')
-grafo.agregar_arista('Plaza Tapatía', 'Parque Revolución')
 
-# Búsqueda en anchura (BFS)
-inicio = 'CETI'
-objetivo = 'Rotonda de los Jaliscienses Ilustres'
-camino = grafo.bfs(inicio, objetivo)
-print(f"Camino BFS de {inicio} a {objetivo}: {camino}")
+# Agregar nodos con distancias
+grafo.agregar_arista('CETI', 'Plaza de la Liberación', 2)
+grafo.agregar_arista('CETI', 'Teatro Degollado', 4)
+grafo.agregar_arista('Plaza de la Liberación', 'Templo Expiatorio', 3)
+grafo.agregar_arista('Teatro Degollado', 'Templo Expiatorio', 2)
+grafo.agregar_arista('Teatro Degollado', 'Rotonda de los Jaliscienses Ilustres', 1)
+grafo.agregar_arista('Templo Expiatorio', 'Rotonda de los Jaliscienses Ilustres', 5)
+grafo.agregar_arista('Instituto Cultural Cabañas', 'Plaza de la Liberación', 1)
+grafo.agregar_arista('Instituto Cultural Cabañas', 'Plaza Tapatía', 2)
+grafo.agregar_arista('Plaza Tapatía', 'Parque Revolución', 3)
+
+# Búsqueda en anchura (BFS) con inicio y objetivo diferentes
+inicio = 'Instituto Cultural Cabañas'
+objetivo = 'Templo Expiatorio'
+camino, distancia = grafo.bfs(inicio, objetivo)
+print(f"Camino BFS de {inicio} a {objetivo}: {camino}, Distancia: {distancia}")
